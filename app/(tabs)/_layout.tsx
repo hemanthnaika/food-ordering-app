db42b72f5fb54b28227524f1886347d732fac671
hemanthnaika <hemanthnaika22@gmail.com>
2025-10-02 16:46:02 +0530

    Set up the base file & folder

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Redirect, Slot } from "expo-router";
+import React from "react";
+
+export default function _layout() {
+  const isAuth = false;
+  if (!isAuth) return <Redirect href="/sign-in" />;
+  return <Slot />;
+}
